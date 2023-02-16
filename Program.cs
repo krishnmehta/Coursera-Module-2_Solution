9ca93ac88ca374b2e0c430df78ec77c709e34dac
krishnmehta <87222503+krishnmehta@users.noreply.github.com>
2023-02-16 17:59:04 +0530

    Update

--- Program.cs
+++ Program.cs
@@ -1,106 +1,79 @@
 using System;
-
-namespace EmpAssignment // Note: actual namespace depends on the project name.
+using System.Linq;
+using System.Collections.Generic;
+					
+public class Program
 {
-    public class Employee
-    {
-        private int Id { get; set; }
-        private string Name { get; set; }
-        private string DepartmentName { get; set; }
+	IList employeeList;
+	IList salaryList;
+	
+	public Program(){
+		employeeList = new List() { 
+			new Employee(){ EmployeeID = 1, EmployeeFirstName = "Rajiv", EmployeeLastName = "Desai", Age = 49},
+			new Employee(){ EmployeeID = 2, EmployeeFirstName = "Karan", EmployeeLastName = "Patel", Age = 32},
+			new Employee(){ EmployeeID = 3, EmployeeFirstName = "Sujit", EmployeeLastName = "Dixit", Age = 28},
+			new Employee(){ EmployeeID = 4, EmployeeFirstName = "Mahendra", EmployeeLastName = "Suri", Age = 26},
+			new Employee(){ EmployeeID = 5, EmployeeFirstName = "Divya", EmployeeLastName = "Das", Age = 20},
+			new Employee(){ EmployeeID = 6, EmployeeFirstName = "Ridhi", EmployeeLastName = "Shah", Age = 60},
+			new Employee(){ EmployeeID = 7, EmployeeFirstName = "Dimple", EmployeeLastName = "Bhatt", Age = 53}			
+		};
+		
+		salaryList = new List() {
+			new Salary(){ EmployeeID = 1, Amount = 1000, Type = SalaryType.Monthly},
+			new Salary(){ EmployeeID = 1, Amount = 500, Type = SalaryType.Performance},
+			new Salary(){ EmployeeID = 1, Amount = 100, Type = SalaryType.Bonus},
+			new Salary(){ EmployeeID = 2, Amount = 3000, Type = SalaryType.Monthly},
+			new Salary(){ EmployeeID = 2, Amount = 1000, Type = SalaryType.Bonus},
+			new Salary(){ EmployeeID = 3, Amount = 1500, Type = SalaryType.Monthly},
+			new Salary(){ EmployeeID = 4, Amount = 2100, Type = SalaryType.Monthly},
+			new Salary(){ EmployeeID = 5, Amount = 2800, Type = SalaryType.Monthly},
+			new Salary(){ EmployeeID = 5, Amount = 600, Type = SalaryType.Performance},
+			new Salary(){ EmployeeID = 5, Amount = 500, Type = SalaryType.Bonus},
+			new Salary(){ EmployeeID = 6, Amount = 3000, Type = SalaryType.Monthly},
+			new Salary(){ EmployeeID = 6, Amount = 400, Type = SalaryType.Performance},
+			new Salary(){ EmployeeID = 7, Amount = 4700, Type = SalaryType.Monthly}
+		};
+	}
+	
+	public static void Main()
+	{		
+		Program program = new Program();
+		
+		program.Task1();
+		
+		program.Task2();
+		
+		program.Task3();
+	}
+	
+	public void Task1(){
+		//Implementation
+	}
+	
+	public void Task2(){
+		 //Implementation
+	}
+	
+	public void Task3(){
+		 //Implementation
+	}
+}
 
-        public delegate void MethodCalledEventHandler(string message); //using delegate
-        public event MethodCalledEventHandler MethodCalled;
+public enum SalaryType{
+	Monthly,
+	Performance,
+	Bonus
+}
 
-        public Employee(int id, string name, string departmentName)
-        {
-            Id = id;
-            Name = name;
-            DepartmentName = departmentName;
-        }
+public class Employee{
+	public int EmployeeID { get; set; }
+	public string EmployeeFirstName { get; set; }
+	public string EmployeeLastName { get; set; }
+	public int Age { get; set; }	
+}
 
-        public int GetId()
-        {
-            //When method called GetId() will print
-            OnMethodCalled("GetId() method called");
-            return Id;
-        }
-
-        public string GetName()
-        {
-            //GetName() will print
-            OnMethodCalled("GetName() method called");
-            return Name;
-        }
-
-        public string GetDepartmentName()
-        {
-            //GetDepartment will print
-            OnMethodCalled("GetDepartmentName() method called");
-            return DepartmentName;
-        }
-
-        public void UpdateEmployee(int id)
-        {
-            Id = id;
-        }
-
-        public void UpdateEmployee(string name)
-        {
-            Name = name;
-        }
-
-        public void UpdateDepartment(string departmentName)
-        {
-            DepartmentName = departmentName;
-        }
-
-        protected virtual void OnMethodCalled(string message)
-        {
-            if (MethodCalled != null)
-                MethodCalled(message);
-        }
-    }
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            //User will enter details
-            Console.WriteLine("Enter Emp ID: ");
-            int id = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter Emp Name: ");
-            string name = Console.ReadLine();
-
-            Console.WriteLine("Enter Emp Department Name: ");
-            string departmentName = Console.ReadLine();
-
-            Employee emp = new Employee(id, name, departmentName);
-            emp.MethodCalled += MethodCalledEvent;
-
-            Console.WriteLine("Emp ID: " + emp.GetId());
-            Console.WriteLine("Emp Name: " + emp.GetName());
-            Console.WriteLine("Emp Department Name: " + emp.GetDepartmentName());
-
-            //When you want to Update Employee Details
-            Console.WriteLine("Update Employee Deatils: ");
-
-            //Entering all the three values to upate the details
-            Console.WriteLine("Enter updated id, name and Department Name: ");
-            id = Convert.ToInt32(Console.ReadLine());
-            name = Console.ReadLine();
-            departmentName = Console.ReadLine();
-            emp.UpdateEmployee(id); // Updating id
-            emp.UpdateEmployee(name);// Updating name
-            emp.UpdateDepartment(departmentName); // Updating department name
-
-            Console.WriteLine("Updated Details are: ");// Printing Updating Details
-            Console.WriteLine(emp.GetId()+ " "+ emp.GetName()+ " "+ emp.GetDepartmentName());
-
-
-            static void MethodCalledEvent(string message)
-            {
-                Console.WriteLine("\n" + message);
-            }
-        }
-    }
+public class Salary{
+	public int EmployeeID { get; set; }
+	public int Amount { get; set; }
+	public SalaryType Type { get; set; }
 }
